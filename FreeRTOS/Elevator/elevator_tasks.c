e6714dc35b40fd77191f7132a04e6a31701329f5
Asif Sardar <engr.asif.sardar@gmail.com>
2019-11-04 16:16:20 +0100

    Add elevator type definitions and task implementation

--- FreeRTOS/Elevator/elevator_tasks.c
+++ FreeRTOS/Elevator/elevator_tasks.c
@@ -4,5 +4,19 @@
  *  Created on: 4 nov. 2019
  *      Author: A551226
  */
+
+/* Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
 #include "elevator_types.h"
 
+/* Priorities at which the tasks are created. */
+#define mainQUEUE_RECEIVE_TASK_PRIORITY   ( tskIDLE_PRIORITY + 2 )
+#define mainQUEUE_SEND_TASK_PRIORITY    ( tskIDLE_PRIORITY + 1 )
+
+/*
+ * The tasks for the
+ */
+static void prvQueueReceiveTask( void *pvParameters );
+static void prvQueueSendTask( void *pvParameters );
--- FreeRTOS/Elevator/elevator_types.h
+++ FreeRTOS/Elevator/elevator_types.h
@@ -8,6 +8,56 @@
 #ifndef ELEVATOR_TYPES_H_
 #define ELEVATOR_TYPES_H_
 
+typedef enum {
+  downward = 0,
+  upward
+} CabinMotorDirection;
 
+typedef enum {
+  notRunning = 0,
+  running
+} MotorState;
+
+typedef struct CABINMOTORCONTROL {
+  CabinMotorDirection motorDirection;
+  MotorState motorState;
+  BaseType_t speed; // rotations/seconds
+} CabinMotorControl_t;
+
+typedef enum {
+  off = 0,
+  on
+} ButtonState;
+
+typedef enum {
+  atGroundFloor = 0,
+  atFirstFloor,
+  atSecondFloor
+} CabinLocation;
+
+typedef struct BUTTON {
+  ButtonState buttonState;
+};
+
+typedef struct CABINBUTTONCONTROL {
+  BUTTON groundFloor;
+  BUTTON firstFloor;
+  BUTTON secondFloor;
+} CabinButtonControl_t;
+
+typedef struct LOBBYBUTTONCONTROL {
+  BUTTON upward;
+  BUTTON downward;
+} LobbyButtonControl_t;
+
+typedef enum {
+  close = 0,
+  open
+} DoorState;
+
+typedef struct LOBBYDOORMOTORCONTROL {
+  DoorState doorState;
+  MotorState motorState;
+} LobbyDoorMotorControl_t;
 
 #endif /* ELEVATOR_TYPES_H_ */
