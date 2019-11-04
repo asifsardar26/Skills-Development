5cdfc31cbee919f37078e9d6f675251ad4a2f6a4
Asif Sardar <engr.asif.sardar@gmail.com>
2019-11-04 15:32:44 +0100

    Elevator demo project with windows RTOS simulator.

---
+++ FreeRTOS/Elevator/elevator_tasks.c
@@ -0,0 +1,8 @@
+/*
+ * elevator_tasks.c
+ *
+ *  Created on: 4 nov. 2019
+ *      Author: A551226
+ */
+#include "elevator_types.h"
+
---
+++ FreeRTOS/Elevator/elevator_types.h
@@ -0,0 +1,13 @@
+/*
+ * elevator_types.h
+ *
+ *  Created on: 4 nov. 2019
+ *      Author: A551226
+ */
+
+#ifndef ELEVATOR_TYPES_H_
+#define ELEVATOR_TYPES_H_
+
+
+
+#endif /* ELEVATOR_TYPES_H_ */
